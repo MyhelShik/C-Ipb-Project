5637b4763ddcd8481c3ad57a33cf284406f1872b
Pedro Santos <pedrosatos2006@gmail.com>
2024-12-03 16:33:29 +0000

    Update main.c
    asdasd

--- main.c
+++ main.c
@@ -3,4 +3,10 @@
 int main()
 {
     printf("Hello, from C-school-project!\n");
+    printf("Sou gay");
+    //asdasd
+    // sadasd
+    // mudanca no codigo
+    //proximo 
+    //sadsaddasdas
     }

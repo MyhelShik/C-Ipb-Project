ebd28332554dff8c4b5d6d1fcc5ce779d6f7bc68
Pedro Santos <pedrosatos2006@gmail.com>
2024-12-03 16:50:52 +0000

    Update main.c
    Estruturas done

--- main.c
+++ main.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    int id;
+    char titulo[100], autor[100];
+    int ano[50];
+    int disponivel; //1 para disponivel e 0 se emprestado
+}livro;
+
+typedef struct 
+{ 
+    int id; 
+    char nome[50]; 
+    char endereco[100]; 
+    char telefone[15]; 
+    } leitor;
+
+typedef struct 
+{ 
+    int idLivro; 
+    int idLeitor; 
+    char dataEmprestimo[11]; 
+    char dataDevolucao[11]; 
+} emprestimo;
+
+
 
 int main()
 {

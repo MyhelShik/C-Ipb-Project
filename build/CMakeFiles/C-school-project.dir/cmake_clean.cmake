a50cfbfaba000cb1d8b701393d5d3146e6678c20
MyhelShik <79665375+MyhelShik@users.noreply.github.com>
2024-12-03 10:10:21 +0000

    initial commit
    Compiler libs CMAKE quickstart

---
+++ build/CMakeFiles/C-school-project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C-school-project.exe"
+  "C-school-project.exe.manifest"
+  "C-school-project.pdb"
+  "CMakeFiles/C-school-project.dir/main.c.obj"
+  "CMakeFiles/C-school-project.dir/main.c.obj.d"
+  "libC-school-project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C-school-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

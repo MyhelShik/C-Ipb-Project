4f3a5c9058e96a4a9ec88daad034cf8e143e8152
MyhelShik <79665375+MyhelShik@users.noreply.github.com>
2024-12-03 16:27:46 +0000

    new push
    asd

--- main.c
+++ main.c
@@ -3,14 +3,4 @@
 int main()
 {
     printf("Hello, from C-school-project!\n");
-    printf("Sou gay");
-    //asdasd
-    // sadasd
-    // mudanca no codigo
-<<<<<<< Updated upstream
-    //proximo 
-=======
-
-    // not mudanca
->>>>>>> Stashed changes
     }

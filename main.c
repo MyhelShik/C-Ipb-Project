2387e6b507de1dda62e05d5d06b6de5811cbc7fb
MyhelShik <79665375+MyhelShik@users.noreply.github.com>
2024-12-20 16:35:06 +0000

    UPDATE MAIN.C

--- main.c
+++ main.c
@@ -10,5 +10,21 @@ int main()
     // GerirLeitores();
     // exibirRelatorioLeitoresAtivos();
     return 0;
+
+
+    FILE *f; 
+    f = fopen("..\\test.txt", "w");
+    leitor cls[] = max_leitor;
+
+    for (int i = 0; i < max_leitor; i++)
+    {
+        fprintf(f, "%d, %s %d\n",leitores[i]->id != NULL, leitores[i]->nome != "", leitores[i]->cc);
+    }
     
+    fclose(f);
+
+    //while(fscanf(f,"%d,%[^\n]",&leitor[i]->id,&leitor.nome)!=EOF> || )
+
+    //fprintf(f, "%d, %s\n",leitor[i]->id != NULL, leitor[i]->nome != "")
+
 }
